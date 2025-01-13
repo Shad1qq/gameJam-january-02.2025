74b331f9f818cddb1b780b6dd5fb8e2274d83341
Shad1qq <124492203+Shad1qq@users.noreply.github.com>
2025-01-13 17:28:59 +0300

    сделал прототип атаки, фиксы, фичекат)))

--- Assets/Scripts/BossFightSL2/BossManager.cs
+++ Assets/Scripts/BossFightSL2/BossManager.cs
@@ -3,25 +3,30 @@ using UnityEngine;
 public class BossManager : MonoBehaviour
 {
     RightHand rightHand = new RightHand();
+    LeftHand leftHand = new LeftHand();
+
     [SerializeField] float handSpeed;
+    [SerializeField] float lefthandSpeed;
+    public static bool handIsWork = false;
     private void Start()
     {
         InitRH();  
     }
     private void Update()
     {
-        ChangeValue(handSpeed);
-        if (ArenaControllerForTwoScene.coolDown < -1f)
+        ChangeValue(handSpeed, lefthandSpeed);
+        if (ArenaControllerForTwoScene.coolDown < -1f && handIsWork == false)
         {
             FollowPlayer();
         }
         
     }
     private void InitRH() => rightHand.InitRH();
-    private void FollowPlayer() => rightHand.FollowPlayer();
-    private void ChangeValue(float handSpeed)
+    private void FollowPlayer() => rightHand.MoveSet();
+    private void ChangeValue(float handSpeed, float lefthandSpeed)
     {
         rightHand.ChangeSpeed(handSpeed);
+        leftHand.ChangeSpeed(lefthandSpeed);
     }
 
 
--- Assets/Scripts/BossFightSL2/RightHand.cs
+++ Assets/Scripts/BossFightSL2/RightHand.cs
@@ -5,17 +5,30 @@ public class RightHand
     private GameObject rightHand;
     private float handSpeed = 3;
     Vector3 offset = new Vector3(0, 5f, 0);
+    float coolDown = 5;
     
     public void InitRH()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         rightHand = GameObject.Find("righthand");
     }
-    public void FollowPlayer()
+    public void MoveSet()
     {
         rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, playerTransform.position + offset, handSpeed * Time.deltaTime);
-        Debug.Log("This Work");
+        if(coolDown < 0)
+        {
+            BossManager.handIsWork = true;
+            Attack();
+        }
+        else BossManager.handIsWork = false;
+        coolDown -= Time.deltaTime;
     }
+
+    private void Attack()
+    {
+        rightHand.transform.position = new Vector3(playerTransform.position.x,playerTransform.position.y, playerTransform.position.z);
+    }
+    
     public void ChangeSpeed(float hS)
     {
         if(hS > 0)
